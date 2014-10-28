8603b0850da1d46953080a5755af987652adfdcb
zhuyuecai <zhuyuecai@gmail.com>
2014-10-28 13:51:30 -0400

    2ed

--- Assignemt_2_352/src/myNodeList/NodeList.java
+++ Assignemt_2_352/src/myNodeList/NodeList.java
@@ -1,23 +1,36 @@
 package myNodeList;
-import java.util.Iterator;
 import myNodeList.Position;
 import myNodeList.emptyListException;
 import myNodeList.inValidInputException;
-public class NodeList implements Iterable<Position> {
+public class NodeList {
     private Position[] list;
     private char expand;
     private int size;
-    private int f,l;
+    private int counter;
+    //default constructor
     public NodeList(){
     	this.list=new Position[10];
     	this.expand='d';
     	this.size=10;
-    	this.f=0;
-    	this.l=0;
+    	this.counter=0;
+    	
     }
+    //constructor with an input of an array of elements
+    public NodeList(int[] eList){
+    	counter=eList.length;
+    	this.list=new Position[10];
+    	while (counter>=size){
+    	    this.expansion();  
+    	}
+    	for(int i =0;i<counter;++i){
+    		this.addLast(eList[i]);
+    	}
+    }
+    //get expand rule
 	public char getExpand() {
 		return expand;
 	}
+	//set up the expansion rule manually 
 	public void setExpansionRule(char expand) throws inValidInputException {
 		if(expand == 'd'||expand == 'c'){
 		this.expand = expand;
@@ -25,12 +38,37 @@ public class NodeList implements Iterable<Position> {
 			throw (new inValidInputException("wrong input, input should be either 'd' or 'c' !"));
 		}
 	}
-	//todo: add expansion rule
-	public Position addFirst(int e){
-		list[l] = new Position(e,list[f+1]);
-		return list[f];
+	//return the total number of elements stored in the list
+	public int getNumOfElements(){
+		return counter;
 	}
-	
+	// and a Position with the input element in the first place of the array and return the Position 
+	public Position addFirst(int e){
+		if (counter>=size){this.expansion();}
+		for(int i = counter-1;i>=0;--i){
+			
+		}
+		list[0] = new Position(e,0);
+		return list[0];
+	}
+	//return the Position in the first place of the array
+	public Position first() throws emptyListException{
+		if ((f+1+size)%size==l){throw new emptyListException("the list is empty!");}
+		else{return list[(f+1+size)%size];}
+	}
+	//return the Position in the last place of the array
+	public Position last() throws emptyListException{
+		if ((f+1+size)%size==l){throw new emptyListException("the list is empty!");}
+		else{return list[(l-1+size)%size];}
+	}
+	//
+	public Position addLast(int e){
+		if (f==l){this.expansion();}
+		list[l] = new Position(e);
+		f=(l+1+size)%size;
+		return list[(l-1+size)%size];
+	}
+/*	
 	@Override
 	public Iterator<Position> iterator() {
 		// TODO Auto-generated method stub
@@ -51,7 +89,8 @@ public class NodeList implements Iterable<Position> {
 		};
 		return it;
 	}
-	
+	*/
+	//todo: expand the double end circular array 
 	private void expansion(){
 		Position[] newList;
 		if (this.expand == 'd'){
@@ -60,10 +99,8 @@ public class NodeList implements Iterable<Position> {
 		else{
 			newList = new Position[this.size*2];
 		}
-		int counter =0;
-		for(Position i : this){
-			newList[counter++]=i;
-		}
+		
+        
 		this.list=newList;
 	}
     
--- Assignemt_2_352/src/myNodeList/Position.java
+++ Assignemt_2_352/src/myNodeList/Position.java
@@ -7,12 +7,33 @@ public class Position {
     	value=0;
     	rank=0;
     }
-    public Position(int value,int rank){
+    /*
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Position other = (Position) obj;
+		if (rank != other.rank)
+			return false;
+		return true;
+	}
+	*/
+	public Position(int value,int rank){
     	this.value = value;
     	this.rank=rank;
     }
     public int element(){
     	return this.value;
+    }
+    protected void setRank(int newRank){
+    	this.rank=newRank;
+    }
+    protected int getRank(){
+    	return rank;
     }
 
 }
